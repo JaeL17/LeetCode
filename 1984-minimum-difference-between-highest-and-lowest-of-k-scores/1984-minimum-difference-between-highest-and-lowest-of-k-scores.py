8891264f13b50e765f1e078299c66d1b304d4200
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-01-25 20:58:56 +0000

    Time: 7 ms (76.41%), Space: 19.5 MB (16.86%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -7,20 +7,6 @@ class Solution:
             best = min(best, nums[i+k-1]-nums[i])
         return best
 
-        if k == 1:
-            return 0
-
-        sorted_list = sorted(nums)
-        min_diff = sorted_list[-1] - sorted_list[0]
-        for i in range(len(nums)):
-            
-            new_list = sorted_list[i:i+k]
-            if len(new_list) < k:
-                break
-            min_diff = min(min_diff, new_list[-1] - new_list[0])
-
-        return min_diff
-
 
         
         
