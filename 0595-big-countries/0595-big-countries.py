89b00bb38a3aff7b5c1fcf34acf959df84113da7
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-17 23:44:11 +0900

    Time: 263 ms (65.6%), Space: 69.3 MB (5.64%) - LeetHub

---
+++ 0595-big-countries/0595-big-countries.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    answer = world[(world["area"] >= 3000000) | (world["population"] >= 25000000)]
+
+    return answer[["name", "area", "population"]]
+    
