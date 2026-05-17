8b167dfa3e7249c1c7edcfa0de2128fc996c0824
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-18 02:18:55 +0900

    Time: 250 ms (93.48%), Space: 66.7 MB (45.88%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.py
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
 def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
-    return patients[patients["conditions"].str.contains(" DIAB1") | patients["conditions"].str.startswith("DIAB1")]
+    condition = (
+    patients["conditions"].str.contains(" DIAB1") |
+    patients["conditions"].str.startswith("DIAB1")
+)
+
+    return patients.loc[condition]
