7a0bb6cdc56a794f686dd4115c565ac8c64357be
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-18 01:22:12 +0900

    Time: 308 ms (65.13%), Space: 68.4 MB (6.15%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.py
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.py
@@ -1,10 +1,15 @@
 import pandas as pd
 
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
-    id_and_bonus = []
-    for emp_id, name, salary in zip(employees["employee_id"], employees["name"], employees["salary"]):
-        if emp_id % 2 ==1 and name[0] != "M":
-            id_and_bonus.append([emp_id, salary])
-        else:
-            id_and_bonus.append([emp_id, 0])
-    return pd.DataFrame(id_and_bonus, columns = ["employee_id", "bonus"]).sort_values("employee_id")
+    # id_and_bonus = []
+    # for emp_id, name, salary in zip(employees["employee_id"], employees["name"], employees["salary"]):
+    #     if emp_id % 2 ==1 and name[0] != "M":
+    #         id_and_bonus.append([emp_id, salary])
+    #     else:
+    #         id_and_bonus.append([emp_id, 0])
+    employees["bonus"] = 0 
+    employees.loc[(employees["employee_id"] % 2 == 1) & ~employees["name"].str.startswith("M"), ["bonus"]] = employees["salary"]
+
+
+    # return pd.DataFrame(id_and_bonus, columns = ["employee_id", "bonus"]).sort_values("employee_id")
+    return employees[["employee_id", "bonus"]].sort_values("employee_id")
