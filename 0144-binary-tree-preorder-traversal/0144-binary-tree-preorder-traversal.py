30b6220a71cfbf4e2856ffbc5f9bd5a32ee54064
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-19 19:46:46 +0900

    Time: 0 ms (100%), Space: 19.1 MB (92.4%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -4,22 +4,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        answer = []
-        stack = deque()
-        stack.append((root, False)) # push (root node, visited state) to the stack.
-        while stack:
-            curr, visited = stack.pop()
-            if curr: # if the node exists
-                if visited:
-                    answer.append(curr.val)
-                else:
-                    stack.append((curr.right, False))
-                    stack.append((curr.left, False))
-                    stack.append((curr, True))
+        # root -> left -> right
+        result = []
+        def dfs(node):
+            if node is None:
+                return
+            result.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
 
-
-        return answer
+        return result
         
