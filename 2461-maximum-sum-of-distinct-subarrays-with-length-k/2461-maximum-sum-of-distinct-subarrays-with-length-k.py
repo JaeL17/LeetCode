2b2ba26fc2abda5b4faec105bd8fd261cee0f187
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-20 12:49:39 +0900

    Time: 123 ms (49.82%), Space: 37.2 MB (60.94%) - LeetHub

---
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        max_sum = 0
+        left = 0
+        curr_sum =0
+        count = {}
+        for right in range(len(nums)):
+
+            # update curr_sum 
+            curr_sum += nums[right]
+            
+            # update counter
+            if nums[right] in count:
+                count[nums[right]] +=1
+            else:
+                count[nums[right]] = 1
+
+            # move left index if the subarray size is greater than k    
+            if right - left + 1 > k:
+                curr_sum -= nums[left] # update current sum
+                count[nums[left]] -=1 # update counter
+                if count[nums[left]] == 0: # if there is no nums[left] element in counter, remove it
+                    del count[nums[left]]
+                left +=1
+            
+            # if subarray size == k and the elements are dinstinct
+            if right - left + 1 == k and len(count) == k:
+                max_sum = max(max_sum, curr_sum)
+
+
+        return max_sum
