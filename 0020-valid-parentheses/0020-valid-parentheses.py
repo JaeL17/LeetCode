6afe95d3acad2001e1f9f1b69b35b36ec7bae46a
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-21 17:38:28 +0900

    Time: 0 ms (100%), Space: 19.4 MB (27.36%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,24 +1,18 @@
-from collections import deque
 class Solution:
     def isValid(self, s: str) -> bool:
-        # (), {}, []
-        stack = deque()
-        check = {")": "(",
-                 "}": "{",
-                 "]": "["}
-        for char in s:
-            if char not in check.keys():
-                stack.append(char)
+        pare_dict: dict = {")" : "(", 
+                           "}" : "{",
+                           "]" : "["}
+        stack: List[str] = []
+
+        # for loop: O(n)
+        for par in s:
+            if par in pare_dict and stack: # if par is a closing parentheses
+                last_par = stack.pop() # O(1)
+                if last_par != pare_dict[par]:
+                    return False
             else:
-                if len(stack) == 0:
-                    return False
-
-                bracket = stack.pop()
-                if bracket != check[char]:
-                    return False
+                stack.append(par) # O(1)
         
-        if len(stack) ==0:
-            return True
-        else:
-            return False
-
+        return True if len(stack) == 0 else False
+        
