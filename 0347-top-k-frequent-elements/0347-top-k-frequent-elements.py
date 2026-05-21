1f6091f617f1391263eeacfa54d5e7905f177bd8
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-21 17:20:11 +0900

    Time: 3 ms (89.02%), Space: 22.9 MB (62%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,22 +1,31 @@
+from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter: dict[int, int] = dict()
-        n = len(nums)
-        
-        # for loop: O(n) 
-        for i in range(n):
-            num = nums[i] # O(1)
 
-            if num in counter:
-                counter[num] +=1 # O(1)
-            else:
-                counter[num] = 1 # O(1)
-
-        # for loop: O(k) 
-        lst: List[Tuple[int, int]] = [(counter[key], key) for key in counter.keys()]
+        count = Counter(nums)
+        count = count.most_common()
+        return [i[0] for i in count[:k]]
+        # counter: dict[int, int] = dict()
+        # n = len(nums)
         
-        # python sorting: O(k*log k) 
-        lst.sort(reverse=True)
-        lst = lst[:k]
-        return [i[1] for i in lst]
+        # # for loop: O(n) 
+        # for i in range(n):
+        #     num = nums[i] # O(1)
+
+        #     if num in counter:
+        #         counter[num] +=1 # O(1)
+        #     else:
+        #         counter[num] = 1 # O(1)
+
+        # # for loop: O(m) 
+        # lst: List[Tuple[int, int]] = [(counter[key], key) for key in counter.keys()]
+
+        # # python sorting: O(m*log m) 
+        # lst.sort(reverse=True)
+
+        # # list slicing: O(k) 
+        # lst = lst[:k]
+
+        # # total time complexity: O(n) + O(m) + O(m*log m) + O(k) =>  O(n + m log m), worst: O(n*log n)
+        # return [i[1] for i in lst]
         
