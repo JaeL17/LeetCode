655dc0fca06a2ac3d59d492550e6e050dd4f6647
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-21 17:20:27 +0900

    Time: 3 ms (89.02%), Space: 22.9 MB (42.44%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,30 +2,31 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
-        count = Counter(nums)
-        count = count.most_common()
-        return [i[0] for i in count[:k]]
-        # counter: dict[int, int] = dict()
-        # n = len(nums)
+        # count = Counter(nums)
+        # count = count.most_common()
+        # return [i[0] for i in count[:k]]
+
+        counter: dict[int, int] = dict()
+        n = len(nums)
         
-        # # for loop: O(n) 
-        # for i in range(n):
-        #     num = nums[i] # O(1)
+        # for loop: O(n) 
+        for i in range(n):
+            num = nums[i] # O(1)
 
-        #     if num in counter:
-        #         counter[num] +=1 # O(1)
-        #     else:
-        #         counter[num] = 1 # O(1)
+            if num in counter:
+                counter[num] +=1 # O(1)
+            else:
+                counter[num] = 1 # O(1)
 
-        # # for loop: O(m) 
-        # lst: List[Tuple[int, int]] = [(counter[key], key) for key in counter.keys()]
+        # for loop: O(m) 
+        lst: List[Tuple[int, int]] = [(counter[key], key) for key in counter.keys()]
 
-        # # python sorting: O(m*log m) 
-        # lst.sort(reverse=True)
+        # python sorting: O(m*log m) 
+        lst.sort(reverse=True)
 
-        # # list slicing: O(k) 
-        # lst = lst[:k]
+        # list slicing: O(k) 
+        lst = lst[:k]
 
-        # # total time complexity: O(n) + O(m) + O(m*log m) + O(k) =>  O(n + m log m), worst: O(n*log n)
-        # return [i[1] for i in lst]
+        # total time complexity: O(n) + O(m) + O(m*log m) + O(k) =>  O(n + m log m), worst: O(n*log n)
+        return [i[1] for i in lst]
         
