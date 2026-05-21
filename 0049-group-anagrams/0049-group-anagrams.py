a5e74550dfcdc1b141e59a8ff7d43366541b5990
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-21 15:46:33 +0900

    Time: 8 ms (92.58%), Space: 21.8 MB (93.06%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,10 +2,14 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagram_counter: dict[str, List[str]] = dict()
         n = len(strs)
+
+        # n = number of strings in strs
+        # k = average length of each string
+        # time: O(n * k log k), space: O(n * k)
         for i in range(n):
-            sorted_str = ''.join(sorted(strs[i]))
-            if sorted_str in anagram_counter:
-                anagram_counter[sorted_str].append(strs[i])
+            sorted_str = ''.join(sorted(strs[i])) # O(k log K) time complexity
+            if sorted_str in anagram_counter: # O(1)
+                anagram_counter[sorted_str].append(strs[i]) # O(1)
             else:
                 anagram_counter[sorted_str] = [strs[i]]
         return [anagram_counter[key] for key in anagram_counter.keys()]
