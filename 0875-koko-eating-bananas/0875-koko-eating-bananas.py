12ae332d0c9ced142329be7c1fa666f82817acb7
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-22 13:34:58 +0900

    Time: 164 ms (72.94%), Space: 20.6 MB (80.84%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,28 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        # binary search between min and max hours possible 
+        left = 1
+        right = max(piles)
+
+        def can_finish(k: int) -> bool:
+            hours = 0
+            for pile in piles:
+                # ceil division: how many hours needed for this pile
+                hours += math.ceil(pile/k)
+            return hours <= h
+
+        answer = right
+        # Time complexity: O(n log m)
+        while left <= right:
+            mid = (left + right) // 2
+
+            if can_finish(mid):
+                answer = mid
+                right = mid -1
+            else:
+                left = mid + 1
+
+
+        return answer
