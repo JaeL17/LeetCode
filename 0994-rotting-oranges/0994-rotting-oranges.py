417f2c83e9fd4db69cc5ef613b65bb2e2b49ad39
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-22 14:08:21 +0900

    Time: 17 ms (5.86%), Space: 19.3 MB (79.07%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
 
@@ -7,11 +8,11 @@ class Solution:
 
         def bfs(pos_min: Tuple[int, int, int]) -> None:
             visited = [[False for _ in range(cols)] for _ in range(rows)]
-            queue : List[Tuple[int, int, int]] = [pos_min]
+            queue : List[Tuple[int, int, int]] = deque([pos_min])
             visited[pos_min[0]][pos_min[1]] = True
 
             while queue:
-                curr_r, curr_c, curr_t = queue.pop(0)   
+                curr_r, curr_c, curr_t = queue.popleft()   
                 for move in moves:
                     new_r, new_c = curr_r + move[0], curr_c + move[1]
                     if 0 <= new_r < rows and 0 <= new_c < cols:
