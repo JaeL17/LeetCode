202881d5138a8f458a65628995beeb86f4ee0abb
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-22 17:39:44 +0900

    Time: 644 ms (51.16%), Space: 22.3 MB (54.21%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,38 +1,36 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        answer = []
-        nums.sort()
-        hash_ = {}
-        for i in range(len(nums)):
-            left_idx = i+1
-            right_idx = len(nums)-1
-            
-            while left_idx < right_idx: # and :
-                sum_ = nums[i]+ nums[left_idx] + nums[right_idx]
-
-                if sum_ == 0:
-                    if str(nums[i]) + str(nums[left_idx]) + str(nums[right_idx]) not in hash_.keys():                
-                        answer.append([nums[i], nums[left_idx], nums[right_idx]])
-                        hash_[str(nums[i]) + str(nums[left_idx]) + str(nums[right_idx])] = ""
-                    left_idx = left_idx +1
-                    right_idx = right_idx - 1
-                elif sum_ < 0:
-                    left_idx = left_idx+1
-                else:
-                    right_idx = right_idx - 1
-                # if nums[left_idx] == nums[right_idx]:
-                #     break
-
-
-            # for j in range(i+1, len(nums)):
-            #     for k in range(j+1, len(nums)):
-            #         if nums[i] + nums[j] + nums[k] == 0 and i != j and k != j and k != i:
-            #             if str(nums[i]) + str(nums[j]) + str(nums[k]) not in hash_.keys():                
-            #                 answer.append([nums[i], nums[j], nums[k]])
-            #                 hash_[str(nums[i]) + str(nums[j]) + str(nums[k])] = ""
-
-        # answer = list(set(answer))
-        return answer
-
-
         
+        # O (nlogn)
+        nums.sort()
+        answer = []
+
+        n = len(nums)
+
+        # O(n^2)
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+                total = nums[i] + nums[right] + nums[left]
+                if total == 0:
+                    answer.append([nums[i], nums[right], nums[left]])
+
+                    while left < right and nums[left] == nums[left + 1]:
+                        left +=1
+
+                    while left < right and nums[right] == nums[right -1]:
+                        right -=1
+
+                    left +=1
+                    right -=1
+                elif total < 0:
+                    left +=1
+                else:
+                    right -=1
+
+        return answer
