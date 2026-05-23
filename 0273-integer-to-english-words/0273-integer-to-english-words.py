cd496b3b351b1b789ad30d6c52236121ea26e004
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-24 01:23:33 +0900

    Time: 0 ms (100%), Space: 19.3 MB (81.81%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.py
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -17,17 +17,20 @@ class Solution:
             # e.g. 957 -> Nine Hundred Fifty Seven
             return (self.numberToWords(num // 100 ) + " " + after_hundred[0] + " " + (self.numberToWords(num % 100) if num % 100 else "")).strip()
         
-        elif num < 1_000_000: # million
-            # e.g. 123957 -> One Hundred Twenty Three Thousand Nine Hundred Fifty Seven
-            return (self.numberToWords(num // 1000 ) + " " + after_hundred[1] + " " + (self.numberToWords(num % 1000) if num % 1000 else "")).strip()
+        # after this point after_hundred index increase by one for every 1000 factor. and the number for opertaion also increases by # 1000 factor. 
+        for i in range(1, len(after_hundred)):
+            val = 1000 ** i
+            if num < val * 1000:
+                return (self.numberToWords(num // val ) + " " + after_hundred[i] + " " + (self.numberToWords(num % val) if num % val else "")).strip()
+
+        # elif num < 1_000_000: # million
+        #     # e.g. 123957 -> One Hundred Twenty Three Thousand Nine Hundred Fifty Seven
+        #     return (self.numberToWords(num // 1000 ) + " " + after_hundred[1] + " " + (self.numberToWords(num % 1000) if num % 1000 else "")).strip()
         
-        elif num < 1_000_000_000: # Billion
+        # elif num < 1_000_000_000: # Billion
 
-            return ((self.numberToWords(num // 1_000_000 ) + " " + after_hundred[2] + " " + (self.numberToWords(num % 1_000_000) if num % 1_000_000 else ""))).strip()
-        else:
-
-            # 1,234,567,891 -> one billion two hundred thirty four million five hundred sisty seven thousand eight hundred ninty one 
-            
-            return ((self.numberToWords(num // 1_000_000_000 ) + " " + after_hundred[3] + " " + (self.numberToWords(num % 1_000_000_000) if num % 1_000_000_000 else ""))).strip()
+        #     return ((self.numberToWords(num // 1_000_000 ) + " " + after_hundred[2] + " " + (self.numberToWords(num % 1_000_000) if num % 1_000_000 else ""))).strip()
+        # else:
+        #     return ((self.numberToWords(num // 1_000_000_000 ) + " " + after_hundred[3] + " " + (self.numberToWords(num % 1_000_000_000) if num % 1_000_000_000 else ""))).strip()
 
         
