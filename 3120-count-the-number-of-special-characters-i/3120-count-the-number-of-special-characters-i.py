bf2de5afcaa61c5a88e86500ad27d881609468d2
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-26 15:03:20 +0900

    Time: 0 ms (100%), Space: 19.3 MB (64.16%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
@@ -1,10 +1,19 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
         
-        alphabet = "abcdefghijklmnopqrstuvwxyz"
-        answer = 0
-        for char in alphabet:
-            if char in word and char.upper() in word:
-                answer +=1
-        return answer
-        
+        # alphabet = "abcdefghijklmnopqrstuvwxyz"
+        # answer = 0
+        # for char in alphabet:
+        #     if char in word and char.upper() in word:
+        #         answer +=1
+        # return answer
+        lower = set()
+        upper = set()
+
+        # O(n) time complexity 
+        for char in word:
+            if char.islower():
+                lower.add(char)
+            else:
+                upper.add(char.lower())
+        return len(lower & upper)
