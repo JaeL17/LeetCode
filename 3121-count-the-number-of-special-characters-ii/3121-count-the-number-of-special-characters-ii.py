f7360e67f8e67e9ab8810ddfb01e082c1924a046
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-27 23:56:11 +0900

    Time: 334 ms (32.94%), Space: 21.2 MB (86.47%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        last_lower = {}
+        first_upper = {}
+
+        for i, char in enumerate(word):
+            c = char.lower()
+
+            if char.islower():
+                last_lower[c] = i
+            else:
+                if c not in first_upper:
+                    first_upper[c] = i
+
+        answer = 0
+
+        for c in last_lower:
+            if c in first_upper and last_lower[c] < first_upper[c]:
+                answer += 1
+
+        return answer
