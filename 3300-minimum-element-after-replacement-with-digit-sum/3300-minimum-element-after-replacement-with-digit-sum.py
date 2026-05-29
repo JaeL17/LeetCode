1e6fea36538f60aefdeb3778ecfb097921f4decf
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-05-29 18:45:40 +0900

    Time: 3 ms (80.35%), Space: 19.3 MB (56.38%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
@@ -1,19 +1,23 @@
 class Solution:
     def minElement(self, nums: List[int]) -> int:
-        answer: List[int] = []
+        answer = float('inf')
 
         def digit_sum(num: int) -> int:
-            num_str = str(num)
-            digit_sum = 0
-            for num_char in num_str:
-                num_int = int(num_char)
-                digit_sum += num_int % 10
+            # num_str = str(num)
+            # digit_sum = 0
+            # for num_char in num_str:
+            #     num_int = int(num_char)
+            #     digit_sum += num_int % 10
 
-            return digit_sum
+            total = 0
+            while num > 0:
+                total += num % 10
+                num //= 10
+            return total
 
-        # O (n * m), n is the length of input list: nums, m is the length of total digits in the input list
+        # O (n), n is the length of input list.
         for num in nums:
-            answer.append(digit_sum(num))
+            answer = min(answer, digit_sum(num))
 
-        return min(answer)
+        return answer
         
