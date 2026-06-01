2dd25c930876f7270df41d6c4a9f079d9dfef524
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-06-01 17:24:34 +0900

    Time: 59 ms (51.58%), Space: 22.2 MB (76.3%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,24 +1,26 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack: List[int] = []
-        operation = "+"
+        stack : List[int] = []
         current_num = 0
+        operation = "+"
 
         for i, char in enumerate(s):
             if char.isdigit():
-                current_num = current_num*10 + int(char) # digit shifting to get number that is more than one digit
-
+                current_num = current_num * 10 + int(char)
+            
             if (not char.isdigit() and char != " ") or i == len(s) - 1:
                 if operation == "+":
-                    stack.append(int(current_num))
+                    stack.append(current_num)
                 elif operation == "-":
-                    stack.append(-int(current_num))
+                    stack.append(-current_num)
                 elif operation == "*":
-                    prev_integer = stack.pop()
-                    stack.append(prev_integer * int(current_num))
+                    prev_num = stack.pop()
+                    stack.append(prev_num * current_num)
                 elif operation == "/":
-                    prev_integer = stack.pop()
-                    stack.append(int(prev_integer / current_num))
+                    prev_num = stack.pop()
+                    stack.append(int(prev_num / current_num))
+
+                current_num = 0
                 operation = char
-                current_num = 0                
+
         return sum(stack)
