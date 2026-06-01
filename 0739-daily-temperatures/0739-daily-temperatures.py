b11c1e72959fc98b4e2c320923cbac3fa54d547b
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-06-02 00:54:02 +0900

    Time: 92 ms (64.25%), Space: 28.6 MB (50.63%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,15 +1,15 @@
-from collections import deque
+from typing import List
+
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        answer = [0]*len(temperatures)
-        stack = [] # store indicies
+        answer: List[int] = [0] * len(temperatures)
+        stack: List[int] = []
 
-        for i in range(len(temperatures)):
-            while stack and temperatures[i] > temperatures[stack[-1]]:
-                idx = stack.pop()
-                answer[idx] = i - idx
+        for i, temperature in enumerate(temperatures):
+            while stack and temperature > temperatures[stack[-1]]:
+                previous_day = stack.pop()
+                answer[previous_day] = i - previous_day
 
             stack.append(i)
 
         return answer
-   
