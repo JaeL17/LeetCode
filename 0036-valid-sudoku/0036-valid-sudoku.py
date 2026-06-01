a2f6192153ade172d599340f8aa9b53f86d37666
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-06-02 01:12:47 +0900

    Time: 3 ms (76.4%), Space: 18.7 MB (99.97%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        # check row & col rule
+        for i in range(9):
+            row = board[i]
+            row = [j for j in row if j != "."]
+            col = [board[j][i] for j in range(9) if board[j][i] != "."]
+
+            if len(row) != len(set(row)) or len(col) != len(set(col)):
+                return False
+
+            if i % 3 == 0:
+                sub_box1 = board[i][: 3] + board[i + 1][: 3] + board[i + 2][: 3]
+                sub_box1 = [j for j in sub_box1 if j != "."]
+
+                sub_box2 = board[i][3: 6] + board[i + 1][3: 6] + board[i + 2][3: 6]
+                sub_box2 = [j for j in sub_box2 if j != "."]
+
+                sub_box3 = board[i][6: ] + board[i + 1][6: ] + board[i + 2][6: ]
+                sub_box3 = [j for j in sub_box3 if j != "."]
+
+                if len(sub_box1) != len(set(sub_box1)) or len(sub_box2) != len(set(sub_box2)) or len(sub_box3) != len(set(sub_box3)):
+                    return False
+
+        return True
+        
