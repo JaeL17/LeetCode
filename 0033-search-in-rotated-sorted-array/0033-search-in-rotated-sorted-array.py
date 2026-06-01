df86a68ecb69e50dc10e6c60fcc122624dccbc5a
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-06-01 18:39:06 +0900

    Time: 0 ms (100%), Space: 19.5 MB (39.62%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -2,27 +2,28 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        min_val_idx = 0
-
-        for i, num in enumerate(nums):
-            if num < nums[min_val_idx]:
-                min_val_idx = i
-
-        sorted_nums = nums[min_val_idx:] + nums[:min_val_idx]
-
+        
         left = 0
-        right = len(sorted_nums) - 1
+        right = len(nums) -1
 
         while left <= right:
-            mid = (left + right) // 2
+            mid = (right + left) // 2
 
-            if sorted_nums[mid] == target:
-                return (mid + min_val_idx) % len(nums)
+            if nums[mid] == target:
+                return mid
 
-            elif sorted_nums[mid] < target:
-                left = mid + 1
+            # left / mid / right
 
-            else:
-                right = mid - 1
+            # left side is sorted:
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]: # target is inside this half
+                    right = mid -1
+                else:
+                    left = mid +1
 
+            else: # right side is sorted
+                if nums[mid] < target <= nums[right]:
+                    left = mid +1
+                else:
+                    right = mid -1
         return -1
