7586aa4270196aab62a524fa7ab443f21f9e9bba
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-06-02 14:22:26 +0900

    Time: 253 ms (25.58%), Space: 19.4 MB (56.98%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
@@ -1,24 +1,34 @@
+from typing import List
+
 class Solution:
-    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
-        time_list: List[int] = []
+    def earliestFinishTime(
+        self,
+        landStartTime: List[int],
+        landDuration: List[int],
+        waterStartTime: List[int],
+        waterDuration: List[int]
+    ) -> int:
+        
+        answer = float("inf")
 
-        def find_time(ride1_start: List[int], duration1: List[int], ride2_start: List[int], duration2: List[int]) -> None:
+        def check_order(
+            ride1_start: List[int],
+            ride1_duration: List[int],
+            ride2_start: List[int],
+            ride2_duration: List[int]
+        ) -> None:
+            nonlocal answer
 
-            for i, start_time1 in enumerate(ride1_start):
-                ride1_duration = duration1[i]
-                ride1_end_time = start_time1 + ride1_duration
+            for i in range(len(ride1_start)):
+                ride1_end_time = ride1_start[i] + ride1_duration[i]
 
-                for j, start_time2 in enumerate(ride2_start):
-                    if start_time2 <= ride1_end_time:
-                        ride2_end_time = ride1_end_time + duration2[j]
-                    else:
-                        ride2_end_time = start_time2 + duration2[j]
-                    time_list.append(ride2_end_time)
+                for j in range(len(ride2_start)):
+                    ride2_actual_start = max(ride1_end_time, ride2_start[j])
+                    ride2_end_time = ride2_actual_start + ride2_duration[j]
 
-        # land -> water
-        find_time(landStartTime, landDuration, waterStartTime, waterDuration)
+                    answer = min(answer, ride2_end_time)
 
-        # land -> water
-        find_time(waterStartTime, waterDuration, landStartTime, landDuration)
+        check_order(landStartTime, landDuration, waterStartTime, waterDuration)
+        check_order(waterStartTime, waterDuration, landStartTime, landDuration)
 
-        return min(time_list)
+        return answer
