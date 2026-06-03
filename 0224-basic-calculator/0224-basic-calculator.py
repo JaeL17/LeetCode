c3136863fcbaac0e5d8980ccfdf17ed6d29e8ff2
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-06-03 16:11:03 +0900

    Time: 43 ms (48.15%), Space: 20.3 MB (64.28%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,15 +1,16 @@
 class Solution:
     def calculate(self, s: str) -> int:
 
-        result: int = 0
+        result : int = 0
         stack: List[int] = []
-        sign: int = 1
         current_number: int = 0
+        sign: int = 1
+
         for char in s:
             if char.isdigit():
                 current_number = current_number * 10 + int(char)
-
-            elif char == "+":
+            
+            if char == "+":
                 result += sign * current_number
                 current_number = 0
                 sign = 1
@@ -18,11 +19,10 @@ class Solution:
                 result += sign * current_number
                 current_number = 0
                 sign = -1
-
+            
             elif char == "(":
                 stack.append(result)
                 stack.append(sign)
-
                 result = 0
                 sign = 1
 
@@ -30,10 +30,51 @@ class Solution:
                 result += sign * current_number
                 current_number = 0
 
-                previous_sign = stack.pop()
-                previous_result = stack.pop()
+                prev_sign = stack.pop()
+                prev_result = stack.pop()
 
-                result = previous_result + previous_sign * result
+                result = prev_result + prev_sign * result
 
         result += sign * current_number
+            
         return result
+
+
+
+
+        # result: int = 0
+        # stack: List[int] = []
+        # sign: int = 1
+        # current_number: int = 0
+        # for char in s:
+        #     if char.isdigit():
+        #         current_number = current_number * 10 + int(char)
+
+        #     elif char == "+":
+        #         result += sign * current_number
+        #         current_number = 0
+        #         sign = 1
+
+        #     elif char == "-":
+        #         result += sign * current_number
+        #         current_number = 0
+        #         sign = -1
+
+        #     elif char == "(":
+        #         stack.append(result)
+        #         stack.append(sign)
+
+        #         result = 0
+        #         sign = 1
+
+        #     elif char == ")":
+        #         result += sign * current_number
+        #         current_number = 0
+
+        #         previous_sign = stack.pop()
+        #         previous_result = stack.pop()
+
+        #         result = previous_result + previous_sign * result
+
+        # result += sign * current_number
+        # return result
