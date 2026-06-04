e6231dce4f09905ef4859a00ec3abad0b0d74e27
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-06-04 13:04:50 +0900

    Time: 446 ms (17.63%), Space: 19.3 MB (32.95%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.py
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.py
@@ -2,19 +2,13 @@ class Solution:
     def totalWaviness(self, num1: int, num2: int) -> int:
         waviness: int = 0
         
+        # time complexity: O (n*m)
         for num in range(num1, num2 + 1):
-
-            if num > 100: # the number is at least 3 digits
-                char = str(num)
+            char = str(num)
+            if len(char) >= 3: # the number is at least 3 digits
                 for i in range(1, len(char)-1):
-                    # if i - 1 > 0 and i + 1 < len(char):
-
                     # check for peak
-                    if int(char[i]) > int(char[i-1]) and  int(char[i]) > int(char[i+1]):
+                    if (int(char[i]) > int(char[i-1]) and  int(char[i]) > int(char[i+1])) or (int(char[i]) < int(char[i-1]) and  int(char[i]) < int(char[i+1])):
                         waviness +=1
                     
-                    # check for valley
-                    elif int(char[i]) < int(char[i-1]) and  int(char[i]) < int(char[i+1]):
-                        waviness +=1
-
         return waviness
