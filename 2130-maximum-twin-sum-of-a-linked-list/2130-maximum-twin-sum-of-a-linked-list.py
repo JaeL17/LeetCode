b506573d39ac3c215208babb24e8f3f399238d44
Jaeyun Lee <73643391+JaeL17@users.noreply.github.com>
2026-06-14 15:01:52 +0900

    Time: 98 ms (11.4%), Space: 68.2 MB (5.03%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -3,24 +3,21 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-from math import ceil
+import math
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
         counter: int = 0
-        value_dict: dict[int, int] = dict()
+        value_dict: dict[int, int] = {}
         node = head
 
         while node:
-            # current_val = node.val
             value_dict[counter] = node.val
             node = node.next
-            counter +=1
+            counter += 1
+
         max_sum = float("-inf")
-     
-        for i in range(math.ceil((counter-1)/2)):
-            max_sum = max(max_sum, value_dict[i] + value_dict[counter-1-i])
+
+        for i in range(counter // 2):
+            max_sum = max(max_sum, value_dict[i] + value_dict[counter - 1 - i])
+
         return max_sum
-
-
-        # print(value_dict)
-        
